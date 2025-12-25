2467b9df9cc5a0222072058663cdae68c53d2743
Anatole Dufour <konixy.p@gmail.com>
2025-12-25 19:50:13 +0100

    updated to the new implementation

--- src/routes/[...email]/+page.server.ts
+++ src/routes/[...email]/+page.server.ts
@@ -1,8 +1,9 @@
 import { Renderer } from 'better-svelte-email';
 import { emailList, createEmail, sendEmail } from 'better-svelte-email/preview';
 import { env } from '$env/dynamic/private';
+import layoutCSS from '../layout.css?raw';
 
-const renderer = new Renderer({}, 'src/routes/layout.css');
+const renderer = new Renderer({ customCSS: layoutCSS });
 
 export function load() {
 	const emails = emailList({
